c69196cea177a96b4017b5349553d82e91d2ef09
Noelle <73260931+No767@users.noreply.github.com>
2023-07-13 16:39:37 -0700

    Fix tests

--- bot/libs/cache/__init__.py
+++ bot/libs/cache/__init__.py
@@ -1,6 +1,5 @@
 from .cp_manager import XeltCPM
 from .decorators import cached, cachedJson
-from .global_cp import xeltCP
 from .key_builder import CommandKeyBuilder
 from .xelt_cache import XeltCache
 
@@ -8,7 +7,6 @@ __all__ = [
     "CommandKeyBuilder",
     "XeltCache",
     "XeltCPM",
-    "xeltCP",
     "cached",
     "cachedJson",
 ]
--- tests/redis/test_xelt_cp_manager.py
+++ tests/redis/test_xelt_cp_manager.py
@@ -1,27 +1,36 @@
 import sys
 from pathlib import Path
 
-from redis.asyncio.connection import ConnectionPool
-
-path = Path(__file__).parents[2].joinpath("bot")
+path = Path(__file__).parents[2].joinpath("Bot")
 sys.path.append(str(path))
 
-from libs.cache import XeltCPManager
+import pytest
+from libs.cache import XeltCPM
+from redis.asyncio.connection import ConnectionPool
 
 REDIS_URI = "redis://localhost:6379/0"
 
 
-def test_create_cp():
-    xeltCPM = XeltCPManager(uri=REDIS_URI)
-    assert isinstance(xeltCPM.createConnPool(), ConnectionPool)
+@pytest.mark.asyncio
+async def test_cpm():
+    async with XeltCPM(uri=REDIS_URI) as cpm:
+        assert isinstance(cpm, ConnectionPool)
+
+
+def test_creation_cp():
+    xeltCP = XeltCPM(uri=REDIS_URI)
+    connPool = xeltCP.createPool()
+    assert isinstance(connPool, ConnectionPool)
 
 
 def test_get_cp():
-    xeltCPM = XeltCPManager(uri=REDIS_URI)
-    assert isinstance(xeltCPM.getConnPool(), ConnectionPool)
+    xeltCP = XeltCPM(uri=REDIS_URI)
+    connPool = xeltCP.getConnPool()
+    assert isinstance(connPool, ConnectionPool)
 
 
-def test_get_cp_exists():
-    xeltCPM = XeltCPManager(uri=REDIS_URI)
-    xeltCPM.createConnPool()
-    assert isinstance(xeltCPM.getConnPool(), ConnectionPool)
+def test_created_cp():
+    xeltCP = XeltCPM(uri=REDIS_URI)
+    xeltCP.createPool()
+    newConnPool = xeltCP.getConnPool()
+    assert isinstance(newConnPool, ConnectionPool)
